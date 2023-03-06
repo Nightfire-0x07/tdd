5aaf47f1446dee2fd944cbd7bbd3f6ed7099e51e
Nightfire-0x07 <wildernesswavecat@tuta.io>
2023-03-06 15:51:46 -0500

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVistiorTest(unittest.TestCase):
@@ -15,17 +17,30 @@ class NewVistiorTest(unittest.TestCase):
 
         #They notice the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the Test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
         # Sam invited to enter to-do item right away
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
         # Sam types "Code python project" into a text box
-
+        inputbox.send_keys('Code python project')
         # When Sam hits enter, then the page update and 
         # returns Sam's typed message as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Code python project' for row in rows)
+        )
         # There is another textbox to enter an item, so Sam
         # enters "start web server"
-
+        self.fail('Finish the test')
         #The page updates again and shows both items on the list
 
         #Sam sees site has generated unique URL to revisit the saved
