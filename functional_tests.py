608828ae47841406bea6818617bc4a268c866d2c
Nightfire-0x07 <wildernesswavecat@tuta.io>
2023-03-07 12:15:53 -0500

    Added check for table row items

--- functional_tests.py
+++ functional_tests.py
@@ -9,6 +9,11 @@ class NewVistiorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+    
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Sam has heard about a to-do app. They go
@@ -32,15 +37,22 @@ class NewVistiorTest(unittest.TestCase):
         # returns Sam's typed message as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
+        self.check_for_row_in_list_table('1: Code python project')
+        
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn('1: Code python project', [row.text for row in rows])
+       
         # There is another textbox to enter an item, so Sam
         # enters "start web server"
-        self.fail('Finish the test')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('start web server')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         #The page updates again and shows both items on the list
-
+        self.check_for_row_in_list_table('1: Code python project')
+        self.check_for_row_in_list_table('2: start web server')
+        self.fail('Finish the test!')
         #Sam sees site has generated unique URL to revisit the saved
         # to-do list
 
