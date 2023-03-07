19f149405cadab005581860593db43bfff1365b1
Nightfire-0x07 <wildernesswavecat@tuta.io>
2023-03-07 16:59:13 -0500

    added multiple users/unique urls tests for new features

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVistiorTest(LiveServerTestCase):
     def setUp(self):
@@ -10,12 +13,20 @@ class NewVistiorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
     
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return 
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         #Sam has heard about a to-do app. They go
         #to check out its homepage
         self.browser.get(self.live_server_url)
@@ -36,8 +47,7 @@ class NewVistiorTest(LiveServerTestCase):
         # When Sam hits enter, then the page update and 
         # returns Sam's typed message as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Code python project')
+        self.wait_for_row_in_list_table('1: Code python project')
         
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -48,14 +58,57 @@ class NewVistiorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('start web server')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         #The page updates again and shows both items on the list
-        self.check_for_row_in_list_table('1: Code python project')
-        self.check_for_row_in_list_table('2: start web server')
-        self.fail('Finish the test!')
+        self.wait_for_row_in_list_table('1: Code python project')
+        self.wait_for_row_in_list_table('2: start web server')
         #Sam sees site has generated unique URL to revisit the saved
         # to-do list
 
         #Sam visits the unique URL, and the to-do list is saved there
 
         #Same exits the app
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        #Sam starts new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Code python project')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Code python project')
+
+        #sam notices the list has a unique URL
+        sam_list_url = self.browser.current_url
+        self.assertRegex(sam_list_url, '/lists/.+')
+
+        # now a new user, Alice, comes to site
+
+        #We use new browser session to ensure no info
+        # of same's is coming through from cookies, etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Alice visits the homepage. there is not sign
+        # of sam's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Code python project', page_text)
+        self.assertNotIn('start web server', page_text)
+
+        # Alice starts a new list by entering a new itme. 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy almond milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1. Buy almond milk')
+
+        # Alice gets her own unique URL
+        alice_list_url = self.browser.current_url
+        self.assertRegex(alice_list_url, '/lists/.+')
+        self.assertNotEqual(alice_list_url, sam_list_url)
+
+        #again, no trace of sam's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Code python project', page_text)
+        self.assertIn('Buy almond milk', page_text)
+
+        # satisfied, they both go back to sleep
+        self.fail('Finish the test!')
